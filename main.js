8adcaaaefdc0bf14880e253fc4ea09e8688c54b9
DimaManko <mankonat@gmail.com>
2025-04-23 14:17:30 +0400

    frecodecamp if else

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
-// Complete the getAverage function which takes in an array of test scores and returns the average score.
+/* Now the teacher needs your help converting the student score to a letter grade.
 
-// The average is calculated by adding up all the scores and dividing by the total number of scores.
+Complete the getGrade function that takes a number score as a parameter. Your function should return a string representing a letter grade based on the score.
 
+Here are the scores and their corresponding letter grades:
 
-function getAverage(scores) {
-  let sum = 0;
-for (let i = 0; i < scores.length; i++){
-  sum = sum + scores[i];
+Score Range	Grade
+100	"A++"
+90 - 99	"A"
+80 - 89	"B"
+70 - 79	"C"
+60 - 69	"D"
+0 - 59	"F" */
+
+function getGrade(score) {
+  if (score === 100) {
+    return "A++"
+  } else if (score <= 99 && score >= 90) {
+    return "A"
+  } else if (score <= 89 && score >= 80) {
+    return "B"
+  } else if (score <= 79 && score >= 70) {
+    return "C"
+  } else if (score <= 69 && score >= 60) {
+    return "D"
+  } else if (score <= 59 && score >= 0) {
+    return "F"
+  }
+    
 }
-  return sum / scores.length
-  
-}
 
 
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
