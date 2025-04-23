e1c0d786e6b9630225928a268ebaa19a4058f408
DimaManko <mankonat@gmail.com>
2025-04-23 14:29:24 +0400

    freecodecamp task 3

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
-/* Now the teacher needs your help converting the student score to a letter grade.
+/* The teacher is really happy with the program you have created so far. But now they want to have an easy way to check if a student has a passing grade. A passing grade is anything that is not an "F".
 
-Complete the getGrade function that takes a number score as a parameter. Your function should return a string representing a letter grade based on the score.
+Complete the function hasPassingGrade that takes a student score as a parameter. Your function should return true if the student has a passing grade and false if they do not. */
 
-Here are the scores and their corresponding letter grades:
+function getAverage(scores) {
+  let sum = 0;
 
-Score Range	Grade
-100	"A++"
-90 - 99	"A"
-80 - 89	"B"
-70 - 79	"C"
-60 - 69	"D"
-0 - 59	"F" */
+  for (const score of scores) {
+    sum += score;
+  }
+
+  return sum / scores.length;
+}
 
 function getGrade(score) {
   if (score === 100) {
-    return "A++"
-  } else if (score <= 99 && score >= 90) {
-    return "A"
-  } else if (score <= 89 && score >= 80) {
-    return "B"
-  } else if (score <= 79 && score >= 70) {
-    return "C"
-  } else if (score <= 69 && score >= 60) {
-    return "D"
-  } else if (score <= 59 && score >= 0) {
-    return "F"
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
   }
-    
+}
+
+function hasPassingGrade(score) {
+  if (getGrade(score) === "F") {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 
-console.log(getGrade(96));
-console.log(getGrade(82));
-console.log(getGrade(56));
+console.log(hasPassingGrade(100));
+console.log(hasPassingGrade(53));
+console.log(hasPassingGrade(87));
