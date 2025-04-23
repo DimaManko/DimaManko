ee5062ff9c2d6daf369788c6baca838b4efe334f
DimaManko <mankonat@gmail.com>
2025-04-23 14:08:58 +0400

    freecodecamp task

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-/* Напишите программу, которая запрашивает у пользователя число, а затем вычисляет и выводит его квадратный корень. Если пользователь ввел отрицательное число, программа преобразует его в положительное.
+// Complete the getAverage function which takes in an array of test scores and returns the average score.
 
-Sample Input 1:
+// The average is calculated by adding up all the scores and dividing by the total number of scores.
 
-16
-Sample Output 1:
 
-Квадратный корень из 16 равен 4
-*/
+function getAverage(scores) {
+  let sum = 0;
+for (let i = 0; i < scores.length; i++){
+  sum = sum + scores[i];
+}
+  return sum / scores.length
+  
+}
 
-let numb = +prompt("Введите ваш возраст");
 
-let result = Math.sqrt(Math.abs(numb));
-
-console.log(`Квадратный корень из ${Math.abs(numb)} равен ${result}`);
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
