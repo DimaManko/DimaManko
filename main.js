a7f1119bbf05d934adbe4e2d58b9161850476418
DimaManko <mankonat@gmail.com>
2025-04-23 14:45:14 +0400

    eview JavaScript fundamentals like functions, variables, conditionals and more by building a gradebook app

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-/* The teacher is really happy with the program you have created so far. But now they want to have an easy way to check if a student has a passing grade. A passing grade is anything that is not an "F".
+/* 
+Теперь, когда у учителя есть вся необходимая информация, он хочет иметь возможность отправить ученику сообщение с результатами.
 
-Complete the function hasPassingGrade that takes a student score as a parameter. Your function should return true if the student has a passing grade and false if they do not. */
+Заполните функцию studentMsg, указав в качестве параметров totalScores и studentScore. Функция должна возвращать строку, представляющую собой сообщение для студента.
+
+Если студент прошел курс, строка должна иметь следующий формат:
+Замените average-goes-here на среднее значение общего количества баллов. Замените grade-goes-here на оценку ученика.
+
+Советы
+
+Используйте функцию getAverage для получения среднего балла по классу.
+Используйте функцию getGrade, чтобы получить оценку ученика.
+Для создания сообщения используйте конкатенацию строк (+).
+Будьте внимательны к пунктуации и пробелам в сообщении.
+
+Class average: average-goes-here. Your grade: grade-goes-here. You passed the course.
+Class average: average-goes-here. Your grade: grade-goes-here. You failed the course.*/
 
 function getAverage(scores) {
   let sum = 0;
@@ -36,7 +50,9 @@ function hasPassingGrade(score) {
   }
 }
 
-
-console.log(hasPassingGrade(100));
-console.log(hasPassingGrade(53));
-console.log(hasPassingGrade(87));
+function studentMsg(totalScores, studentScore) {
+   let average = getAverage(totalScores);
+   let grade = getGrade(studentScore);
+  return (hasPassingGrade(studentScore) ? `Class average: ${average}. Your grade: ${grade}. You passed the course.` : `Class average: ${average}. Your grade: ${grade}. You failed the course.`);
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
