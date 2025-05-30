e8e563747aef9bf38840ff4b5dfc6f8de5a9367a
DimaManko <mankonat@gmail.com>
2025-05-31 01:03:35 +0300

    Big data object

--- main.js
+++ main.js
@@ -1,83 +1,143 @@
-/* Реализуйте функцию analyzeArtifacts(artifacts), которая будет анализировать коллекцию космических артефактов и выводить результаты. Каждый артефакт передается в виде объекта с уникальным идентификатором, типом, годом создания и количеством показов. Ваша функция должна:
+/* Работа с "большими данными"
+В коде есть переменная data с объектом:
 
-Вывести список всех артефактов в формате:
-Артефакт [ИД]: Тип - [Тип], Год - [Год], Показы - [Количество показов]
-Найти артефакты с наибольшим количеством показов и вывести их информацию.
-Подсчитать, сколько артефактов каждого типа хранится в музее и вывести результат.
-Между каждым заданием, в выводе, должна быть пустая строка.
 
-Пример объекта с данными
-const artifacts = {
-  A101: { type: "метеорит", year: 1890, exhibits: 15 },
-  A102: { type: "луноход", year: 1973, exhibits: 20 },
-  A103: { type: "скафандр", year: 1969, exhibits: 12 },
-  A104: { type: "спутник", year: 1957, exhibits: 20 },
-  A105: { type: "луноход", year: 1971, exhibits: 10 },
-};
+Необходимо вывести в консоль объект, в котором будут свойства:
 
-Пример вызова функции
-analyzeArtifacts(testArtifacts);
-Пример вывода
-Артефакт A101: Тип - метеорит, Год - 1890, Показы - 15
-Артефакт A102: Тип - луноход, Год - 1973, Показы - 20
-Артефакт A103: Тип - скафандр, Год - 1969, Показы - 12
-Артефакт A104: Тип - спутник, Год - 1957, Показы - 20
-Артефакт A105: Тип - луноход, Год - 1971, Показы - 10
-
-Артефакты с наибольшим количеством показов (количество показов 20):
-A102: Тип - луноход, Год - 1973
-A104: Тип - спутник, Год - 1957
-
-Количество артефактов по типу:
-метеорит: 1
-луноход: 2
-скафандр: 1
-спутник: 1
+name - имя пользователя, например "Иван". +
+address - строка вида "Город, Улица", например "Москва, Красная площадь". +
+friends - массив с именами всех друзей, например ["Мария", "Алексей"]. +
+notifications - массив с уведомлениями, которые активны у пользователя, в порядке их упоминания, например ["email", "push"].
+activityTypes - массив, с уникальными значениями всех типов активностей, в порядке их упоминания, без повторов, например ["workout", "reading"].
 Примечание
-Используйте цикл for...in для перебора объектов.
-Обратите внимание на формат вывода: между заданиями должна быть пустая строка. */
+В объекте должны все свойства располагаться (либо должны быть записаны) в том же порядке, который указан в задании.*/
 
-function analyzeArtifacts(artifacts) {
-  let maxView = -Infinity;
-  for (const key in artifacts) {
-    console.log(`Артефакт ${key}: Тип - ${artifacts[key].type}, Год - ${artifacts[key].year}, Показы - ${artifacts[key].exhibits}`);
-    let exhibitsValue = artifacts[key].exhibits;
-    if (maxView < exhibitsValue) {
-      maxView = exhibitsValue;
-    }
-  }
-  console.log();
-  console.log(`Артефакты с наибольшим количеством показов (количество показов ${maxView}):`);
-  for (const key in artifacts) {
-    if (artifacts[key].exhibits === maxView) {
-      const artifact = artifacts[key];
-      console.log(`${key}: Тип - ${artifact.type}, Год - ${artifact.year}`);
-    }
-  }
-  console.log();
-  const typeCounts = {};
-
-for (const key in artifacts) {
-  const type = artifacts[key].type;
-  if (typeCounts[type]) {
-    typeCounts[type]++;
-  } else {
-    typeCounts[type] = 1;
-  }
-}
-
-console.log("Количество артефактов по типу:");
-for (const type in typeCounts) {
-  console.log(`${type}: ${typeCounts[type]}`);
-}
-}
-
-const artifacts = {
-  A101: { type: "метеорит", year: 1890, exhibits: 15 },
-  A102: { type: "луноход", year: 1973, exhibits: 20 },
-  A103: { type: "скафандр", year: 1969, exhibits: 12 },
-  A104: { type: "спутник", year: 1957, exhibits: 20 },
-  A105: { type: "луноход", year: 1971, exhibits: 10 },
+const data = {
+  user: {
+    id: 1,
+    name: "Иван",
+    age: 30,
+    email: "ivan@example.com",
+    address: {
+      street: "Красная площадь",
+      city: "Москва",
+      postalCode: "101000",
+    },
+    hobbies: ["фотография", "путешествия", "чтение"],
+    friends: [
+      {
+        id: 2,
+        name: "Мария",
+        age: 28,
+        interests: ["живопись", "фотография"],
+      },
+      {
+        id: 3,
+        name: "Алексей",
+        age: 32,
+        interests: ["спорт", "музыка", "путешествия"],
+      },
+    ],
+  },
+  settings: {
+    theme: "dark",
+    notifications: {
+      email: true,
+      sms: false,
+      push: true,
+    },
+    language: "ru",
+  },
+  activities: [
+    {
+      type: "workout",
+      date: "2023-10-01",
+      duration: 60,
+      details: {
+        activityType: "бег",
+        distance: 5,
+      },
+    },
+    {
+      type: "reading",
+      date: "2023-10-02",
+      duration: 90,
+      details: {
+        bookTitle: "Война и мир",
+        pagesRead: 50,
+      },
+    },
+  ],
 };
 
-analyzeArtifacts(artifacts);
+const outputObject = {};
+const arrFrnd = [];
+const notificationsArr = [];
+outputObject.name = data.user.name;
+outputObject.address = data.user.address.city + ", " + data.user.address.street;
+const arrFriends = data.user.friends;
+for (let i = 0; i < arrFriends.length; i++) {
+  arrFrnd.push(arrFriends[i].name);
+}
+outputObject.friends = arrFrnd;
+let keysNotification = Object.keys(data.settings.notifications);
+if (data.settings.notifications.email === true) {
+  for (let i = 0; i < keysNotification.length; i++) {
+    if (keysNotification[i] === "email") {
+      notificationsArr.push(keysNotification[i])
+    }
+  }
+} 
+if (data.settings.notifications.sms === true) {
+    for (let i = 0; i < keysNotification.length; i++) {
+    if (keysNotification[i] === "sms") {
+      notificationsArr.push(keysNotification[i])
+    }
+  }
+}
+if (data.settings.notifications.push === true) {
+    for (let i = 0; i < keysNotification.length; i++) {
+    if (keysNotification[i] === "push") {
+      notificationsArr.push(keysNotification[i])
+    }
+  }
+}
+outputObject.notifications = notificationsArr;
+const activitiesSe = data.activities;
+
+function getUniqueTypes(activitiesSe) {
+  const uniqueTypes = [];
+  
+  for (const activity of activitiesSe) {
+    if (!uniqueTypes.includes(activity.type)) {
+      uniqueTypes.push(activity.type);
+    }
+  }
+  
+  return uniqueTypes;
+}
+outputObject.activityTypes = getUniqueTypes(activitiesSe);
+console.log(outputObject);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
