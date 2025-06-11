242bca484aacecf0defb49f4e99152686eb2e0ee
DimaManko <mankonat@gmail.com>
2025-06-11 18:27:38 +0300

    sort array

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function friend(friends){
-  //your code here
- return  friends.filter(elem => elem.length === 4)
-}
+/* Напишите функцию, которая принимает массив строк в качестве аргумента и возвращает отсортированный массив, содержащий те же строки, упорядоченные от самой короткой к самой длинной.
 
-console.log(friend(["Peter", "Stephen", "Joe"]));
+Например, если в качестве аргумента был передан следующий массив:
+
+["Telescopes", "Glasses", "Eyes", "Monocles"]
+Ваша функция вернет следующий массив:
+
+["Eyes", "Glasses", "Monocles", "Telescopes"]
+Все строки в массиве, переданном в вашу функцию, будут разной длины, поэтому вам не придется решать, как упорядочить несколько строк одинаковой длины. */
+
+function sortByLength (array) {
+  // Return an array containing the same strings,
+  // ordered from shortest to longest
+    return array.sort((a, b) => a.length -b.length)
+}
+console.log(sortByLength(["Telescopes", "Glasses", "Eyes", "Monocles", 'dfgdfgdgdgdgdgdfgd', 'fg']));
